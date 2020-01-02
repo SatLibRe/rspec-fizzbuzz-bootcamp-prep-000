00b654081cd64fa26cd5f2cd3ea0a368dc89231b
Miles Higbie <miles.higbie@gmail.com>
2020-01-02 20:01:56 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,11 +3,11 @@
 
 def fizzbuzz(num)
     
-    if num % 3 == 0 
-        return "Fizz"
+    if num % 3 == 0 && num % 5 == 0 
+      
     elsif num % 5 == 0 
       return "Buzz"
-    elsif num % 3 == 0 && num % 5 == 0 
+    elsif num % 3 == 0 
       return "FizzBuzz"
     end 
 end 
